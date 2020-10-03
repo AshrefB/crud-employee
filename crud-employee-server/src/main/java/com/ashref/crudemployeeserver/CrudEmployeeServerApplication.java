63e1396bfdef411fcfe2873c2abdf67d9524f543
Achref Bouadila <a_bouadila@live.fr>
2020-10-03 20:29:40 +0100

    Initial commit

---
+++ crud-employee-server/src/main/java/com/ashref/crudemployeeserver/CrudEmployeeServerApplication.java
@@ -0,0 +1,13 @@
+package com.ashref.crudemployeeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEmployeeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEmployeeServerApplication.class, args);
+	}
+
+}
---
+++ crud-employee-server/src/test/java/com/ashref/crudemployeeserver/CrudEmployeeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashref.crudemployeeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudEmployeeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
